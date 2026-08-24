c789a58ad035b1c421e14f3f7022bab1b37eda1e
Pavan Prajapati <pavanprajapati3004@gmail.com>
2026-08-24 19:43:08 +0530

    Clarify size check in thirdLargest method
    Added a comment to clarify the check for array size.

--- Difficulty: Easy/Third Largest/third-largest.java
+++ Difficulty: Easy/Third Largest/third-largest.java
@@ -1,14 +1,14 @@
 class Solution {
     public int thirdLargest(List<Integer> arr) {
         
-        if(arr.size () <3){
+        if(arr.size () <3){  // check if elements > 3
             return -1;
         }
         int max = Integer.MIN_VALUE;
         int smax = Integer.MIN_VALUE;
         int tmax = Integer.MIN_VALUE;
         
-        for (int x : arr){
+        for (int x : arr){  // Enhanced For Loop
             if (x >= max){
                 tmax = smax;
                 smax = max;
